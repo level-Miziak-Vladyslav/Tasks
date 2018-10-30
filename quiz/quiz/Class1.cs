f1bd56100ec8cdc3d48aa1ae3a08857a0e1bffb1
level-Miziak-Vladyslav <parado9921@gmail.com>
2018-10-30 17:10:17 +0100

    3010

---
+++ quiz/quiz/Class1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace quiz
+{
+    interface IQestion
+    {
+        bool Validation();
+        object Conservation();
+        bool IsDeleted { get; set; }
+    }
+    public abstract class Qestion : IQestion
+    {
+        public string str;
+        public abstract bool Validation();
+        public object Conservation()
+        {
+            return this.MemberwiseClone();
+        }
+        public bool IsDeleted { get; set; }
+    }
+    public class TextAr:Qestion
+    {
+        public override bool Validation()
+        {
+            throw new NotImplementedException();
+        }
+    }
+    public class MandatoryChoice : Qestion
+    {
+        enum E { };
+        int chcked;
+        public override bool Validation()
+        {
+            throw new NotImplementedException();
+        }
+    }
+    public class MultipleChoice : Qestion
+    {
+        enum E { };
+        int[,] chcked = new int[E.GetNames(typeof(E)).Length,2];
+        public override bool Validation()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+
+
+
+
+
+}
